e62a281dbe259c5944fd1fbddc1b53985c25f2f6
Sairaj Patil <sairajpatil035@gmail.com>
2026-04-16 19:22:02 +0530

    chat

--- mongo-express-learning/index.js
+++ mongo-express-learning/index.js
@@ -6,6 +6,8 @@ const Chat = require("./models/chat.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
 .then(() => {
@@ -20,9 +22,49 @@ async function main() {
 
 
 //INDEX ROUTE 
-app.get("/chats",(req, res) => {
-     []
+app.get("/chats", async (req, res) => {
+ let chats = await Chat.find();
+ console.log(chats);
+ res.render("index.ejs",{chats});
+
+});
+
+//NEW ROUTE 
+app.get("/chats/new", (req,res) => {
+    res.render("new.ejs");
+      
 })
+
+//CREATE ROUTE 
+app.post("/chats", (req,res) => {
+    let{from, to, msg} = req.body;
+    let newChat =new Chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date(),
+    });
+    newChat
+    .save()
+    .then((res)=> {
+        console.log("chat was saved");
+    }).catch((err) => {
+        console.log(err);
+    });
+    
+    res.redirect("/chats" ) ;
+});
+
+//EDIT ROUTE 
+app.get("/chats/:id/edit", (req,res) => {
+    res.render("edit.ejs");
+})
+
+
+app.get("/",(req,res) => {
+    res.send("root is working ");
+  
+});
 
 let chat1 = new Chat({
     from:"neha",
@@ -35,9 +77,7 @@ chat1.save().then((res) => {
     console.log(res);
 })
 
-app.get("/",(req,res) => {
-    res.send("root is working ");
-});
+
 
 app.listen(8080, () => {
     console.log("server is listening on port 8080");
